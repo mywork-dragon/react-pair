8654fa30cd74c93081310ada35f970995c47ede2
mywork-dragon <dragonblueyounger@gmail.com>
2020-06-23 01:23:53 +1000

    initial commit

---
+++ react-pair-initial/tools/server.js
@@ -0,0 +1,54 @@
+import express from 'express';  
+import webpack from 'webpack';  
+import path from 'path';  
+import config from '../webpack.config.dev';  
+import open from 'open';  
+import favicon from 'serve-favicon';
+
+
+/* eslint-disable no-console */
+
+const port = 3000;  
+const app = express();  
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {  
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));  
+
+app.get('*', function(req, res) {  
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+const server = app.listen(port, function(err) {  
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
+
+const io = require('socket.io')(server);
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+ 
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('coding event', function(data) {
+    console.log('in EXPRESS coding event')
+    socket.broadcast.emit('receive code', data)
+  })
+  socket.on('change mode', function(data) {
+    socket.broadcast.emit('receive change mode', data)
+  })
+});
+
+
+
+
